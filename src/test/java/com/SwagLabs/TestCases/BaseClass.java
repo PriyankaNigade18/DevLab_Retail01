83b1c650717f5061adc4b5dfaa2a4994215bb07f
PriyankaNigade18 <Piyu1818@gmail.com>
2024-03-23 22:56:58 +0530

    Framework reports

--- src/test/java/com/SwagLabs/TestCases/BaseClass.java
+++ src/test/java/com/SwagLabs/TestCases/BaseClass.java
@@ -8,6 +8,11 @@ import org.testng.annotations.*;
 
 import com.SwagLabs.Pages.*;
 import com.SwagLabs.Utility.PropertiesFile;
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
 
 public class BaseClass
 {
@@ -18,6 +23,29 @@ public class BaseClass
 	public CheckOutPage cp;
 	public OverviewPage op;
 	public PropertiesFile ps;
+	public  ExtentReports extent;
+	public ExtentSparkReporter spark;
+	public  ExtentTest test;
+	
+	@BeforeSuite
+	public void setUpReport()
+	{
+		 extent = new ExtentReports();
+		  //path
+		   spark = new ExtentSparkReporter("./"+"/Report/SWagLabReport.html");
+		  //configuration
+		  spark.config().setDocumentTitle("SwagLab Automation Report");
+		  spark.config().setReportName("SwagLab");
+		  spark.config().setTheme(Theme.DARK);
+		  
+		  //attached
+		  extent.attachReporter(spark);
+		  
+		  //create test
+		  test=extent.createTest("SwagLab Report");
+		  
+	}
+	
 	
 	
 	@BeforeTest
@@ -53,20 +81,25 @@ public class BaseClass
 		System.out.println("*****Login********");
 		System.out.println("Title is: "+lp.getAppTitle());
 		lp.doLogin(ps.getData("un"),ps.getData("psw"));
+		test.log(Status.PASS,"Login test pass!");
 		addWait();
 		System.out.println("******Inventory Page*****");
 		System.out.println("Total Products are: "+ip.getProductCount());
 		addWait();
+		test.log(Status.PASS,"Total Products calculated!");
 		ip.getProductName();
 		ip.addProductToCart(ps.getData("pname1"));
+		test.log(Status.PASS,"Product added to the cart!");
 		addWait();
 		System.out.println("*******Cart Page*********");
 		ap.getCartPage();
 		addWait();
 		ap.doRemoveProduct();
+		test.log(Status.PASS,"Product removed");
 		addWait();
 		ap.doContinueshopping();
 		addWait();
+		test.log(Status.PASS,"Continue to inventory page for shopping");
 		ip.addProductToCart(ps.getData("pname2"));
 		addWait();
 		ap.getCartPage();
@@ -74,11 +107,23 @@ public class BaseClass
 		ap.doCheckout();
 		System.out.println("*******Checkout Page*********");
 		cp.doContinue(ps.getData("fn"),ps.getData("ln"),ps.getData("zc"));
+		test.log(Status.PASS,"CheckOut is completed");
 		System.out.println("*******Overview Page*********");
-
+		test.log(Status.PASS,"Product details and Confirmation is done!");
 		
 		
 		
 		
 	}
+	
+	@AfterSuite
+	public void reportFlush()
+	{
+		extent.flush();
+	}
+	
+	
+	
+	
+	
 }
